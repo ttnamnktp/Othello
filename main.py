a3176389733433e12cf91cb59caa099240660b26
minhhuyentr <taichinh.iday18@gmail.com>
2024-05-17 14:24:58 +0700

    Hoan thanh logic UI nguoi vs nguoi

---
+++ main.py
@@ -0,0 +1,104 @@
+import pygame
+import time
+from ui.ui import *
+from engine.GameState import GameState
+
+WIDTH = 832
+# HEIGHT = 512
+HEIGHT = 640
+
+# C_DIMENSION = 9
+# R_DIMENSION = 11
+# SQ_SIZE = 64
+# MAX_FPS = 10
+# IMAGES = {}
+B_WIDTH = B_HEIGHT = 512  # board_width and board_height
+DIMENSION = 8
+SQ_SIZE = B_HEIGHT // DIMENSION  # square_size
+MAX_FPS = 15
+IMAGES = {}
+
+scenes = {
+    'TITLE': SimpleScene('Cờ lật'),
+    'CHOOSE_MODE': ChooseScene('Chọn chế độ chơi', 'Người Vs Người', 'Người Vs Máy', 'Máy Vs Máy'),
+    # 'CHOOSE_BOT': ChooseBot('Chọn Bot', 'Negamax', 'Negascout', 'Minimax', 'Greedy'),
+    'HELP': HelpScene('Help', 'Your help text here.'),
+    'GAME_STATE': ChessboardScene('Cờ lật', GameState()),
+    'GAME_OVER': None
+}
+
+
+def load_images():
+    pieces = ['W', 'B']
+    for piece in pieces:
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load("ui/image/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    load_images()
+
+    gs = GameState()
+    running = True
+    scene = scenes['TITLE']
+    chess_gui = ChessGUI(gs)
+    game_over_scene = None
+
+    while running:  # Main game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False  # Exit the loop and close the program if the user closes the window
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # Handle mouse clicks
+                if scene == scenes['TITLE']:
+                    result = scene.update([event])  # Pass the event to the scene's update method
+                    if result == 'CHOOSE_MODE':
+                        scene = scenes['CHOOSE_MODE']
+                    elif result == 'HELP':
+                        scene = scenes['HELP']
+                        pass  # Handle help scene
+                elif scene == scenes['CHOOSE_MODE']:
+                    selected_option = scene.element([event])
+                    if selected_option:
+                        scene = scenes['GAME_STATE']
+                        chess_gui.run_game(screen)  # Run the game in ChessGUI
+                        scenes['GAME_OVER'] = GameOver(gs)
+                        scene = scenes['GAME_OVER']
+
+                        # if chess_gui.gs.is_game_over():
+                        #     scenes['GAME_OVER'] = GameOver(gs)
+                        #     scene = scenes['GAME_OVER']
+                        #     break
+                      
+
+
+        screen.fill((0, 0, 0))  # Clear the screen
+        scene.draw(screen)
+
+        
+        # # Test Success
+        # if not gs.is_game_over() and scenes['GAME_OVER'] is None:
+        #     # Simulate the game being over
+        #     gs.black_count = 31
+        #     gs.white_count = 32
+        #     gs.black_pass = True
+        #     gs.white_pass = True
+        # elif scenes['GAME_OVER'] is None:
+        #     scenes['GAME_OVER'] = GameOver(gs)
+        #     game_over_scene = scenes['GAME_OVER']
+        #     print(gs.black_count)
+
+        # if game_over_scene:
+        #     game_over_scene.draw(screen)
+
+        
+
+        pygame.display.flip()  # Update the display
+        clock.tick(MAX_FPS)
+
+    
+    pygame.quit()  # Quit pygame when the loop ends
+
+if __name__ == "__main__":
+    main()
